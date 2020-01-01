4c5a6719b8167e59db611229be15b847854bfe11
juandiego9221 <juandiego9221@gmail.com>
2020-01-01 08:39:26 -0500

    mapeo por list jpa

--- src/main/java/pe/com/jdmm21/demojpa3/app/demojpa3/model2/Person1.java
+++ src/main/java/pe/com/jdmm21/demojpa3/app/demojpa3/model2/Person1.java
@@ -1,6 +1,9 @@
 package pe.com.jdmm21.demojpa3.app.demojpa3.model2;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Column;
@@ -12,6 +15,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.IndexColumn;
 
 @Entity(name = "persona1")
 @Table(name = "person")
@@ -25,10 +30,15 @@ public class Person1 {
     private String personName;
     @Column(name = "personlastname")
     private String personLastname;
-    @ElementCollection(targetClass = java.lang.String.class, fetch = FetchType.EAGER)
+    @ElementCollection(targetClass = java.lang.String.class)
     @Column(name = "direction")
-    @JoinTable(name = "address",joinColumns = @JoinColumn(name = "id"))
+    @JoinTable(name = "address", joinColumns = @JoinColumn(name = "id"))
     private Set addresses = new HashSet<>();
+    @Column(name = "direction")
+    @JoinTable(name = "address2",joinColumns = @JoinColumn(name = "id"))
+    @ElementCollection(targetClass = String.class)
+    @IndexColumn(name = "position")
+    private List addresses2 = new ArrayList<>();
 
     /**
      * @return int return the id
@@ -84,6 +94,20 @@ public class Person1 {
      */
     public void setAddresses(Set addresses) {
         this.addresses = addresses;
+    }
+
+    /**
+     * @return List return the addresses2
+     */
+    public List getAddresses2() {
+        return addresses2;
+    }
+
+    /**
+     * @param addresses2 the addresses2 to set
+     */
+    public void setAddresses2(List addresses2) {
+        this.addresses2 = addresses2;
     }
 
 }
